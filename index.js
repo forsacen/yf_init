7128a7fd28646cee24b2628e0eefd073b29d54b3
forsacen <forsacen@protonmail.com>
2020-12-14 09:17:35 +0800

    update

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const EventEmitter=require('events').EventEmitter
 String.prototype.trimL = function (char) {
     return this.replace(new RegExp('^\\'+char+'+', 'g'), '')
 }
@@ -7,3 +8,27 @@ String.prototype.trimR = function (char) {
 String.prototype.trimD = function (char) {
     return this.replace(new RegExp('^\\'+char+'+|\\'+char+'+$', 'g'), '')
 }
+
+
+
+EventEmitter.prototype.emitAsync=function(event,...args){
+    return new Promise((resolve)=>{
+        if(this._events[event]){
+            if(typeof this._events[event]==='function'){
+                this.emit(event,resolve,...args)
+            }else{
+                let count=this._events[event].length
+                this.emit(event,()=>{
+                    count--
+                    if(count===0){
+                        resolve()
+                    }
+                },...args)
+            }
+        }else{
+            resolve()
+        }
+    })
+}
+
+EventEmitter.prototype.onAsync=EventEmitter.prototype.on
