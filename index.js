8d162f456ef78f6876006d6baaab9a4ef9adb7ed
forsacen <forsacen@protonmail.com>
2020-12-30 10:53:10 +0800

    update

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ String.prototype.trimR = function (char) {
 String.prototype.trimD = function (char) {
     return this.replace(new RegExp('^\\'+char+'+|\\'+char+'+$', 'g'), '')
 }
+//替换所有字符串
+String.prototype.replaceAll=function(src,dest){
+    return this.replace(new RegExp(src,'g'),dest)
+}
 
 //查找2个字符串之间的字符,如果start为'',则从头部查起,没找到返回undefined
 String.prototype.findBetween = function(start,end){
@@ -23,6 +27,36 @@ String.prototype.findBetween = function(start,end){
         return undefined
     }
     return this.substring(s+start.length,e)
+}
+
+//提取中文,extraReg是额外要匹配的正则字符串，例如《|》
+String.prototype.getChinese=function(extraReg) {
+    //匹配中文字符以及这些中文标点符号 。 ？ ！ ， 、 ； ： “ ” ‘ ' （ ） 《 》 〈 〉 【 】 『 』 「 」 ﹃ ﹄ 〔 〕 … — ～ ﹏ ￥
+    let reg = '[\u4e00-\u9fa5|\u3002|\uff1f|\uff01|\uff0c|\u3001|\uff1b|\uff1a|\u201c|\u201d|\u2018|\u2019|\uff08|\uff09|\u300a|\u300b|\u3008|\u3009|\u3010|\u3011|\u300e|\u300f|\u300c|\u300d|\ufe43|\ufe44|\u3014|\u3015|\u2026|\u2014|\uff5e|\ufe4f|\uffe5';
+    if(extraReg){
+        reg=reg+'|'+extraReg
+    }
+    reg+=']'
+    let tempValue = this.match(new RegExp(reg,'g')); //匹配结果，可能为空，还需要进行判断
+    let resultStr="";
+    if(tempValue!=null){
+        resultStr = tempValue.join("");
+    }
+    return resultStr; //返回匹配的字符串
+}
+//提取英文字,extraReg是额外要匹配的正则字符串，例如<|>|\n|\t
+String.prototype.getEnglish=function(extraReg) {
+    let reg = '[a-zA-Z';
+    if(extraReg){
+        reg=reg+'|'+extraReg
+    }
+    reg+=']'
+    let tempValue = this.match(new RegExp(reg,'g')); //匹配结果，可能为空，还需要进行判断
+    let resultStr="";
+    if(tempValue!=null){
+        resultStr = tempValue.join("");
+    }
+    return resultStr; //返回匹配的字符串
 }
 
 
